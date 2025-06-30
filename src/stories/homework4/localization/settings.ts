e69664e6126b7b974d6677d4b8fffe89236fb157
atrofimova <atrofimova@inobitec.com>
2025-06-30 22:08:57 +0300

    fix: remove init options from settings

--- src/stories/homework4/localization/settings.ts
+++ src/stories/homework4/localization/settings.ts
@@ -1,4 +1,4 @@
-import i18n, { InitOptions } from 'i18next';
+import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { resources } from './resources';
 
@@ -14,13 +14,9 @@ i18n
   .init({
     resources,
     lng: Locale.ru,
-    // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-    // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-    // if you're using a language detector, do not define the lng option
-
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
-  } as InitOptions);
+  });
 
 export default i18n;
