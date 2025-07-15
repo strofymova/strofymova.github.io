56ab09dd0ec44b80d95da84cf0851657925b64ed
atrofimova <atrofimova@inobitec.com>
2025-07-15 13:57:40 +0300

    feat: merge from main

--- src/widgets/layout/Layout.tsx
+++ src/widgets/layout/Layout.tsx
@@ -3,6 +3,8 @@ import Header from '../header/Header'
 import styles from './layout.module.css'
 import {Theme, ThemeContext } from '../../app/App'
 import clsx from 'clsx';
+import { ProductList, IProductList } from '../../homework7/product_list/ProductList'
+import { IProduct } from '../marketplace/products/Product';
 
 export function Layout (): React.ReactNode {
     const {theme} = useContext(ThemeContext);
@@ -10,9 +12,29 @@ export function Layout (): React.ReactNode {
     useEffect(() => {
         setStyleName(clsx(styles.main, theme === Theme.light ? styles.dark : styles.light));
     }, [theme]);
+    
+    const varProducts: IProduct[] = [
+            {
+                price: 100,
+                imageUrl: null,
+                name: "Товар 1",
+                description: "Описание товара 1"
+            },
+            {
+                price: 200,
+                imageUrl: "/path/to/image.jpg",
+                name: "Товар 2",
+                description: "Описание товара 2",
+                disable: true
+            }
+        ];
+
+    const className = "productList";
+    
     return (
             <div className={styleName}>
                 <Header/>
+                <ProductList products={varProducts} className={className}/>
             </div>
     )
 }
